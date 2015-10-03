5209785d9c5d1e4d5382f8a8fd88eace3badb39c
Sushrut Girdhari <developer@girdhari.in>
2015-10-03 10:58:05 -0700

    create registration form, update models

--- assignmentGrader/portal/models.py
+++ assignmentGrader/portal/models.py
@@ -5,8 +5,8 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User)
 
     profile_pic = models.ImageField(upload_to='profile_images', blank=True)
-    StaffMember = models.BooleanField(editable=False, default=False)
-    
+    StaffMember = models.BooleanField(default=False)
+
     def __unicode__(self):
         return self.user.username
 
--- assignmentGrader/portal/views.py
+++ assignmentGrader/portal/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django import forms
+from portal import models
+from portal.forms import UserForm, UserProfileForm
 
 # Create your views here.
+def register(request):
+
+    regStatus = False
+
+    if request.method == 'POST':
+        #grab info from raw information
+        user_form=UserForm(data=request.POST)
+        profile_form=UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user=user_form.save()
+        #hash the password for security using the djangohash default method
+        user.set_password(user.password)
+        user.save()
+
+        profile = profile_form.save(commit=False)
+        profile.user = user
+
+        if 'profile_pic' in request.FILES:
+            profile_pic = request.FILES['profile_pic']
+
+            profile.save()
+
+            regStatus = True
+        else:
+            print user_form.errors, profile_form.errors
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+    return render(request, 'register.html', {'user_form': user_form, 'profile_form': profile_form, 'registered': regStatus})
